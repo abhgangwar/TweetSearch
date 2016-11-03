bba774934e9d776f3e0f6f9756a078c04dfc2fa5
Abhinav Gangwar <abhgang@gmail.com>
2016-11-03 20:17:21 +0530

    Added some more comments

--- App.js
+++ App.js
@@ -2,5 +2,8 @@
 const http 			= require('http');
 const reqHandler 	= require('./routes/RequestHandler');
 
+// Create a server
 const server = http.createServer(reqHandler);
+
+// Provide a port on which server will listen for http requests.
 server.listen(3000);
